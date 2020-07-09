ce3183e0878c118cb61c92e2012e99a3026b310d
Krsnananda <krsnananda@outlook.com>
2020-07-09 09:00:22 -0300

    - Exceptions com while e input de usuário

--- exception.rb
+++ exception.rb
@@ -10,3 +10,14 @@ rescue
 else
    puts "a + b is #{a + b}"
 end
+
+while 1
+   puts "Enter a number>>"
+   begin
+     num = Kernel.gets.match(/\d+/)[0]
+   rescue
+     puts "Deixa de ser burro, tem que ser número! Tenta de novo..."
+   else  
+     puts "#{num} + 1 is: #{num.to_i+1}"
+   end  
+end
